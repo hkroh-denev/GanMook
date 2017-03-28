d6a2f5c9fcfb61be6ca9b30e033f25ff82960756
hkroh-denev <hkroh.denev@gmail.com>
2017-03-28 16:28:58 +0900

    adding config file, support high-resolution with tiling

--- pbserve.py
+++ pbserve.py
@@ -8,16 +8,31 @@ import tensorflow as tf
 import web
 import json
 
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--config_file', dest='config_file', default='serve.json', help='name of the config file')
+args = parser.parse_args()
+
+if not os.path.exists('./test'):
+    os.makedirs('./test')
+if os.path.exists(args.config_file):
+    with open(args.config_file) as data_file:
+        config = json.load(data_file)
+else:
+    print(args.config_file, 'is not found. Using default config.')
+    config = []
+
 def main(_):
     print("GanMook serve!")
-    if not os.path.exists('./test'):
-        os.makedirs('./test')
 
-    with tf.gfile.GFile('model/ganmook_clf_2.pb', 'rb') as f:
+    if config:
+        pb_file = config["model_file"]
+    else:
+        pb_file = 'model/ganmook_clf_2.pb'
+
+    with tf.gfile.GFile(pb_file, 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
-
-    print(tf.trainable_variables());
+    print('Loading ', pb_file)
 
     with tf.Graph().as_default() as graph:
         generated,  = tf.import_graph_def(
@@ -34,14 +49,62 @@ def main(_):
         webapp = serve(urls, globals())
         web.generated_result = generated
         web.sess = tf.Session()
-        webapp.run(port=8081)
+        if config:
+            webapp.run(port=config["port"])
+        else:
+            webapp.run(port=8081)
 
-def test_single_image(web, param):
+def image_overlay_avg(master, x, y, subimg):
+    for i in range(3, 250):
+        for j in range(3, 250):
+            if (master[y + j , x + i] == -1.1):
+                master[y + j, x + i] = subimg[j, i]
+            else:
+                master[y + j, x + i] = (master[y + j, x + i] + subimg[j, i]) / 2.0
+
+def test_single_image_full(web, param):
     input_img = scipy.misc.imread('./test/input_{}.jpg'.format(param['id']), flatten = True).astype(np.float)
     h, w = input_img.shape
-    input_img = scipy.misc.imresize(input_img, [256, 256])
+    pad_h = h+ 256 #(h+255) // 256 * 256
+    pad_w = w + 256 #(w+255) // 256 * 256
+    pad_img = np.pad(input_img[:h, :w], pad_width=((0, pad_h-h), (0, pad_w-w)), mode='constant')
+    x = y = n = 0
+    output_img = np.empty((pad_h, pad_w), np.float)
+    output_img.fill(-1.1)
+    while x < w:
+        while y < h:
+            print('x & y', x, y)
+            sub_img = pad_img[y:y+256, x:x+256]
+            sub_img = sub_img / 127.5 - 1.
+            if (np.mean(sub_img) < - 0.999):
+                generated = np.ones_like(sub_img)
+            else:
+                sample_image = np.empty((256, 256, 6), dtype=np.float32)
+                for c in range(6):
+                    sample_image[:, :, c] = sub_img
+                sample_images = [sample_image]
+                generated = web.sess.run(web.generated_result,
+                    feed_dict={'pix2pix/real_A_and_B_images:0': sample_images}
+                )
+            image_overlay_avg(output_img, x, y, np.squeeze(generated))
+            n = n + 1
+            y = y + 63
+        x = x + 63
+        y = 0
+    output_crop_img = output_img[:h, :w]
+    scipy.misc.imsave('./test/output_{}.jpg'.format(param['id']), output_crop_img)
+
+
+def test_single_image(web, param):
+    if config:
+        input_size = config["input_size"]
+    else:
+        input_size = 256
+    input_img = scipy.misc.imread('./test/input_{}.jpg'.format(param['id']), flatten = True).astype(np.float)
+    h, w = input_img.shape
+    input_img = scipy.misc.imresize(input_img, [input_size, input_size])
     input_img = input_img/127.5 - 1.
-    sample_image = np.empty((256, 256, 6), dtype=np.float32)
+    sample_image = np.empty((input_size, input_size, 6), dtype=np.float32)
     for i in range(6):
         sample_image [:, :, i] = input_img
     sample_images = [sample_image]
@@ -65,6 +128,7 @@ class index:
         print(str)
         data = json.loads(str)
         test_single_image(web, data)
+        #test_single_image_full(web, data)
         return '200 OK'
 
 class serve(web.application):
